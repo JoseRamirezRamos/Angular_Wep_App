f0b32ddbbf7253d9120111aa39b342a16cdf887b
desarrollo <desarrollo@gtservice.mx>
2020-06-29 15:14:53 -0700

    create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit , ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatPaginator} from '@angular/material/paginator';
+import { MatTableDataSource} from '@angular/material/table';
+
+export interface Information {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular';
+
+  displayedColumns: string[] = ['userId', 'id', 'title', 'body'];
+  dataSource = new MatTableDataSource<Information>();
+
+  columns = [
+    {title: 'userId', name: "User ID"},
+    {title: 'id', name: "ID"},
+    {title: 'title', name: "Title"},
+    {title: 'body', name: "Body"}
+  ]
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  constructor (private _http: HttpClient) {
+    this._http.get('https://jsonplaceholder.typicode.com/posts').subscribe((data: Information[]) => {
+      this.dataSource.data = data;
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule
+  ],
+  exports: [
+    MatTableModule,
+    MatPaginatorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
